2138ed74c4a112be4f98b374e79b96b80cef4bb2
Luis Diaz <diazldev@gmail.com>
2023-01-26 22:35:03 -0300

    Interaccion con elementos

--- cypress/e2e/platziTestUi/masterInteration.cy.js
+++ cypress/e2e/platziTestUi/masterInteration.cy.js
@@ -40,11 +40,65 @@ describe('',() =>{
             expect(texto).to.equal('Maria Anders')
         })
     })
-    it.only('interation Datepickers',() =>{
+    it('interation Datepickers',() =>{
         cy.visit('https://material.angular.io/components/datepicker/overview')
         cy.get('#mat-mdc-form-field-label-0').type('11/30/1987')
         cy.get('datepicker-overview-example')
         .find('svg').click({force:true})
         cy.get('#mat-datepicker-0').should('be.visible')
+    })
+    it('Interactuando con popups', () => {
+        cy.visit('https://demoqa.com/alerts')
+        //Cypress automaticamente la acepta
+
+        // Primer forma de hacerlo
+        // cy.get('#confirmButton').click()
+        // cy.on('window:confirm', (confirm) => {
+        //     expect(confirm).to.equal('Do you confirm action?')
+        // })
+        // cy.contains('You selected Ok').should('exist')
+
+        // Segundo forma de hacerlo
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('#confirmButton').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Do you confirm action?')
+        })
+        cy.contains('You selected Ok').should('exist')
+
+
+        // rechazar la alerta
+        // cy.get('#confirmButton').click()
+        // cy.on('window:confirm', (confirm) => {
+        //     expect(confirm).to.equal('Do you confirm action?')
+        //     return false
+        // })
+        // cy.contains('You selected Cancel').should('exist')
+
+    })
+
+    it('Interactuando con tooltips', () => {
+        cy.visit('https://demoqa.com/tool-tips')
+        cy.get('#toolTipButton').trigger('mouseover')
+        cy.contains('You hovered over the Button').should('exist')
+        cy.get('#toolTipButton').trigger('mouseout')
+        cy.contains('You hovered over the Button').should('not.exist')
+
+    })
+
+    it.only('Interactuando con drag a drops', () => {
+        cy.visit('https://demoqa.com/dragabble')
+        cy.get('#dragBox')
+        .trigger('mousedown', {
+            which: 1,
+            pageX: 600,
+            pageY: 100,
+        }).trigger('mousemove', {
+            which : 1,
+            pageX : 900,
+            pageY : 800,
+        }).trigger('mouseup')
+        
+
     })
 })
